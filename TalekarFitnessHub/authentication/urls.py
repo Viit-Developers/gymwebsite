08015b879341209724b334abbb0e9feb91b1da95
kiran-kd <kdahake412@gmail.com>
2023-11-25 11:57:25 +0530

    added databse tabel in models.py

--- TalekarFitnessHub/authentication/urls.py
+++ TalekarFitnessHub/authentication/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from authentication import views
+from authapp import views
+
 urlpatterns = [
     path('',views.Home,name="Home"),
-    path('signup',views.signup,name='signup'),
-    path('handlelogin',views.handlelogin,name='handlelogin'),
-    path('logout',views.logout,name='logout')
+    path('signup',views.signup,name="signup"),
+    path('login',views.handlelogin,name="handlelogin"),
+    path('logout',views.handleLogout,name="handleLogout"),
+    path('contact',views.contact,name="contact"),
+    path('join',views.enroll,name="enroll"),
+    path('profile',views.profile,name="profile"),
+    path('gallery',views.gallery,name="gallery"),
+    path('attendance',views.attendance,name="attendance"),
+
+    
 ]
