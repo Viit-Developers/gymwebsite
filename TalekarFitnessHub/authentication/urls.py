6d2a0c1827bb2c98e594cba852fb48bfed4d2f27
kiran-kd <kdahake412@gmail.com>
2023-11-25 11:53:18 +0530

    add login , logout , signup frontend and backend code

--- TalekarFitnessHub/authentication/urls.py
+++ TalekarFitnessHub/authentication/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from authentication import views
 urlpatterns = [
-    path('',views.Home,name="Home")
+    path('',views.Home,name="Home"),
+    path('signup',views.signup,name='signup'),
+    path('handlelogin',views.handlelogin,name='handlelogin'),
+    path('logout',views.logout,name='logout')
 ]
